62b5439790a6a86e5370bca2da78218f045ede1e
albertcent <144157962+albertcent@users.noreply.github.com>
2023-09-07 09:22:07 -0600

    TS 5 ALBERT CENTENO

---
+++ TS5-AC_12208233.py
@@ -0,0 +1,19 @@
+# TS Introducción a la programación sección: 17
+# 07/09/2023
+# Albert Centeno
+# Objetivo 
+# Entrada
+# Proceso
+# Salida
+
+print ("Mi segundo programa")
+sNombre= input ("Ingresar nombre")
+sEdad = input ("Ingresar edad")
+sCarrera = input ("Ingresar su carrera")
+sCarne = input ("Ingresar numero de carne")
+
+print ("Nombre", sNombre)
+print ("Edad", sEdad)
+print ("Carrera", sCarrera)
+print ("Carne", sCarne)
+print ("Soy" + sNombre, "tengo" + sEdad, "años y estudio la carrera de" + sCarrera, ". Mi numero de carné es:" + sCarne)
